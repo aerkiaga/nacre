aebf30f509298661d4d75fd815abdbaeabe6fdc9
Aritz Erkiaga <aerkiaga3@gmail.com>
2024-05-28 21:40:17 +0200

    Generalize cache

--- rooster_parser/src/cache.rs
+++ rooster_parser/src/cache.rs
@@ -2,6 +2,8 @@ use crate::*;
 
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
+use std::future::Future;
+use std::pin::{pin, Pin};
 use std::sync::Arc;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
@@ -9,104 +11,98 @@ use tokio::sync::Notify;
 use tokio::sync::RwLock;
 
 // An entry in the parser cache
-#[derive(Clone)]
-enum CacheEntry {
-    // Notifies tasks when the AST they requested becomes available
+enum CacheEntry<T> {
+    // Notifies tasks when the value they requested becomes available
     Pending(Arc<Notify>),
-    // The (fully processed) AST
-    Available(Arc<parser2::AbstractSyntaxTree>),
+    // The (fully processed) value
+    Available(Arc<T>),
 }
 
-// The global cache storing all processed definitions
-static PARSER_CACHE: Lazy<RwLock<HashMap<String, CacheEntry>>> =
-    Lazy::new(|| HashMap::new().into());
-
-fn error_cannot_find(filename: &str) -> ! {
-    println!("Error: cannot find file '{}'.", filename);
-    panic!();
-}
-
-fn error_cannot_read() -> ! {
-    println!("Error: file has invalid encoding.");
-    panic!();
-}
-
-// TODO: move into new module
-async fn read_file(file: &mut File) -> String {
-    let mut r = String::new();
-    match file.read_to_string(&mut r).await {
-        Ok(_) => (),
-        Err(_) => error_cannot_read(),
-    }
-    r
-}
-
-/// Make sure the AST associated with a logical path is available.
-async fn load_ast(logical_path: &str) -> Result<(), ()> {
-    // TODO: load file only if necessary
-    // TODO: convert logical to physical paths
-    let mut file = match File::open(logical_path).await {
-        Ok(x) => x,
-        Err(_) => error_cannot_find(logical_path),
-    };
-    // TODO: call preprocessor giving it an async stream
-    let ast = preprocess::preprocess_file(&read_file(&mut file).await).await?;
-    println!("{:?}", &ast);
-    PARSER_CACHE
-        .write()
-        .await
-        .insert(logical_path.to_string(), CacheEntry::Available(ast.into()));
-    Ok(())
-}
-
-async fn try_get_ast(logical_path: &str) -> Option<Arc<parser2::AbstractSyntaxTree>> {
-    match PARSER_CACHE.read().await.get(logical_path)? {
-        CacheEntry::Pending(_) => None,
-        CacheEntry::Available(ast) => Some(ast.clone()),
+impl<T> Clone for CacheEntry<T> {
+    fn clone(&self) -> Self {
+        match self {
+            CacheEntry::Pending(notify) => CacheEntry::Pending(notify.clone()),
+            CacheEntry::Available(value) => CacheEntry::Available(value.clone()),
+        }
     }
 }
 
-pub(crate) async fn get_ast(logical_path: &str) -> Result<Arc<parser2::AbstractSyntaxTree>, ()> {
-    // First, atomically check if entry exists and insert pending if not
-    let option = {
-        let notify = Arc::new(Notify::new());
-        match PARSER_CACHE.write().await.try_insert(
-            logical_path.to_string(),
-            CacheEntry::Pending(notify.clone()),
-        ) {
-            Ok(_) => Some(notify),
-            Err(_) => None,
-        }
-    };
+pub(crate) struct Cache<T> {
+    storage: Lazy<RwLock<HashMap<String, CacheEntry<T>>>>,
+    loader: fn(&str) -> Pin<Box<dyn Future<Output = Result<T, ()>> + Send + '_>>,
+}
 
-    // If entry didn't exist (now pending), spawn task to compute it
-    if let Some(notify) = option {
-        let s = logical_path.to_string();
-        let notify_sender = notify.clone();
-        let result = tokio::spawn(async move {
-            load_ast(&s).await?;
-            notify_sender.notify_waiters();
-            Ok::<(), ()>(())
-        })
-        .await
-        .unwrap();
-        if let Err(_) = result {
-            PARSER_CACHE.write().await.remove(logical_path);
-            return Err(());
-        }
-        if let Err(_) = try_get_ast(logical_path).await.ok_or(()) {
-            PARSER_CACHE.write().await.remove(logical_path);
-            return Err(());
+impl<T: Send + Sync> Cache<T> {
+    pub(crate) const fn new(
+        loader: fn(&str) -> Pin<Box<dyn Future<Output = Result<T, ()>> + Send + '_>>,
+    ) -> Cache<T> {
+        Cache {
+            storage: Lazy::new(|| HashMap::new().into()),
+            loader: loader,
         }
     }
 
-    // Wait if pending, return AST if/when available
-    let entry = PARSER_CACHE.read().await.get(logical_path).unwrap().clone();
-    match entry {
-        CacheEntry::Pending(notify) => {
-            notify.notified().await;
-            Ok(try_get_ast(logical_path).await.ok_or(())?)
+    // Make sure the value associated with a key is available.
+    async fn load(&self, key: &str) -> Result<(), ()> {
+        let r = (self.loader)(key).await?;
+        self.storage
+            .write()
+            .await
+            .insert(key.to_string(), CacheEntry::Available(r.into()));
+        Ok(())
+    }
+
+    async fn try_get(&self, key: &str) -> Option<Arc<T>> {
+        match self.storage.read().await.get(key)? {
+            CacheEntry::Pending(_) => None,
+            CacheEntry::Available(val) => Some(val.clone()),
         }
-        CacheEntry::Available(ast) => Ok(ast.clone()),
+    }
+
+    pub(crate) async fn get(&'static self, key: &str) -> Result<Arc<T>, ()> {
+        // First, atomically check if entry exists and insert pending if not
+        let option = {
+            let notify = Arc::new(Notify::new());
+            match self
+                .storage
+                .write()
+                .await
+                .try_insert(key.to_string(), CacheEntry::Pending(notify.clone()))
+            {
+                Ok(_) => Some(notify),
+                Err(_) => None,
+            }
+        };
+
+        // If entry didn't exist (now pending), spawn task to compute it
+        if let Some(notify) = option {
+            let s = key.to_string();
+            let notify_sender = notify.clone();
+            let result = tokio::spawn(async move {
+                self.load(&s).await?;
+                notify_sender.notify_waiters();
+                Ok::<(), ()>(())
+            })
+            .await
+            .unwrap();
+            if let Err(_) = result {
+                self.storage.write().await.remove(key);
+                return Err(());
+            }
+            if let Err(_) = self.try_get(key).await.ok_or(()) {
+                self.storage.write().await.remove(key);
+                return Err(());
+            }
+        }
+
+        // Wait if pending, return value if/when available
+        let entry_clone = self.storage.read().await.get(key).unwrap().clone();
+        match entry_clone {
+            CacheEntry::Pending(notify) => {
+                notify.notified().await;
+                Ok(self.try_get(key).await.ok_or(())?)
+            }
+            CacheEntry::Available(val) => Ok(val.clone()),
+        }
     }
 }
--- rooster_parser/src/lib.rs
+++ rooster_parser/src/lib.rs
@@ -1,5 +1,15 @@
 #![feature(map_try_insert)]
 
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+use std::future::Future;
+use std::pin::{pin, Pin};
+use std::sync::Arc;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+use tokio::sync::Notify;
+use tokio::sync::RwLock;
+
 mod cache;
 mod lexer;
 mod operators;
@@ -11,7 +21,51 @@ mod report;
 pub use report::Report;
 pub use report::REPORTS;
 
+fn error_cannot_find(filename: &str) -> ! {
+    println!("Error: cannot find file '{}'.", filename);
+    panic!();
+}
+
+fn error_cannot_read() -> ! {
+    println!("Error: file has invalid encoding.");
+    panic!();
+}
+
+// TODO: move into new module
+async fn read_file(file: &mut File) -> String {
+    let mut r = String::new();
+    match file.read_to_string(&mut r).await {
+        Ok(_) => (),
+        Err(_) => error_cannot_read(),
+    }
+    r
+}
+
+fn parser_loader(
+    logical_path: &str,
+) -> Pin<Box<dyn Future<Output = Result<parser2::AbstractSyntaxTree, ()>> + Send + '_>> {
+    Box::pin(async move {
+        // TODO: load file only if necessary
+        // TODO: convert logical to physical paths
+        let mut file = match File::open(logical_path).await {
+            Ok(x) => x,
+            Err(_) => error_cannot_find(logical_path),
+        };
+        let ast = preprocess::preprocess_file(&read_file(&mut file).await).await?;
+        println!("{:?}", &ast); //D
+        Ok(ast)
+    })
+}
+
+// The global cache storing all processed definitions
+static PARSER_CACHE: cache::Cache<parser2::AbstractSyntaxTree> =
+    cache::Cache::new(parser_loader as _);
+
+async fn get_ast(key: &str) -> Result<Arc<parser2::AbstractSyntaxTree>, ()> {
+    PARSER_CACHE.get(key).await
+}
+
 /// Get the annotated CoC expression corresponding to a particular logical path.
 pub async fn get_expression(logical_path: &str) {
-    cache::get_ast(logical_path).await.unwrap();
+    get_ast(logical_path).await.unwrap();
 }
