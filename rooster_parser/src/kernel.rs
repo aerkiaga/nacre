360e37cc95686940a8a7b650be5d03385b7268ae
Aritz Erkiaga <aerkiaga3@gmail.com>
2024-05-30 19:03:37 +0200

    Implement CoC term generation

--- rooster_parser/src/kernel.rs
+++ rooster_parser/src/kernel.rs
@@ -1,24 +1,52 @@
 use crate::parser2::AbstractSyntaxTree;
 use crate::*;
 
-use async_recursion::async_recursion;
+use rooster_kernel::Term;
+use std::collections::HashMap;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
 use tokio::task::JoinSet;
 
+// A counter to assign indices to top-level definitions
+static INDEX_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+fn kernel_loader(
+    logical_path: &str,
+) -> Pin<Box<dyn Future<Output = Result<(Arc<Term>, Arc<Term>, usize), ()>> + Send + '_>> {
+    let logical_path_string = logical_path.to_string();
+    Box::pin(async move {
+        println!("Verifying {}", logical_path);
+        // First we compute all dependencies, so the operation can be parallelized
+        let deps = semantics::get_direct_dependencies(logical_path).await?;
+        let mut join_set = JoinSet::new();
+        for dep in deps.iter() {
+            let dep_string = dep.to_string();
+            join_set.spawn(async move { verify(&dep_string).await });
+        }
+        while let Some(result) = join_set.join_next().await {
+            result.or(Err(()))?;
+        }
+        let (ast, filename) = get_ast(logical_path).await.unwrap();
+        let definition =
+            Arc::new(semantics::convert_to_term(ast, &HashMap::new(), 0, &filename).await?);
+        //let type_term = definition.get_type()?; // need environment
+        let index = INDEX_COUNTER.fetch_add(1, Ordering::Relaxed);
+        println!("Verified {}", logical_path);
+        Ok((definition, Arc::new(Term::Prop), index))
+    })
+}
+
+// The global cache storing definition, type and index for each verified definition
+static KERNEL__CACHE: cache::Cache<(Arc<Term>, Arc<Term>, usize)> =
+    cache::Cache::new(kernel_loader as _);
+
 /// Parse and verify the CoC expression corresponding to a particular logical path.
-#[async_recursion]
 pub async fn verify(logical_path: &str) -> Result<(), ()> {
-    println!("Verifying {}", logical_path);
-    let deps = semantics::get_direct_dependencies(logical_path).await?;
-    let mut join_set = JoinSet::new();
-    for dep in deps.iter() {
-        let dep_string = dep.to_string();
-        join_set.spawn(async move { verify(&dep_string).await });
-    }
-    while let Some(result) = join_set.join_next().await {
-        result.or(Err(()))?;
-    }
-    // Perform the verification here!
-    let (ast, filename) = get_ast(logical_path).await.unwrap();
-    println!("Verified {}", logical_path);
+    KERNEL__CACHE.get(logical_path).await?;
     Ok(())
+}
+
+// Get the index associated to a top-level definition
+pub(crate) async fn get_global_index(logical_path: &str) -> Result<usize, ()> {
+    Ok(KERNEL__CACHE.get(logical_path).await?.2)
 }
--- rooster_parser/src/semantics.rs
+++ rooster_parser/src/semantics.rs
@@ -1,6 +1,9 @@
 use crate::parser2::AbstractSyntaxTree;
 use crate::*;
 
+use async_recursion::async_recursion;
+use rooster_kernel::Term;
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 // TODO: use CoW
@@ -70,6 +73,109 @@ fn compute_dependencies(ast: &AbstractSyntaxTree, locals: &HashSet<String>) -> H
                     new_locals.insert(name.clone());
                     compute_dependencies(value, &new_locals)
                 }
+        }
+        _ => panic!(),
+    }
+}
+
+#[async_recursion]
+pub(crate) async fn convert_to_term(
+    ast: &AbstractSyntaxTree,
+    locals: &HashMap<String, usize>,
+    level: usize,
+    filename: &str,
+) -> Result<Term, ()> {
+    match ast {
+        AbstractSyntaxTree::Block(statements, _) => {
+            let mut rv = vec![];
+            let mut new_locals = locals.clone();
+            let mut new_level = level;
+            let mut n = 0;
+            for statement in statements {
+                match &**statement {
+                    AbstractSyntaxTree::Assignment(name, value, is_definition, _) => {
+                        if *is_definition {
+                            if let AbstractSyntaxTree::Identifier(components, _) = &**name {
+                                if components.len() > 1 {
+                                    panic!();
+                                }
+                                rv.push(convert_to_term(value, locals, new_level, filename).await?);
+                                new_locals.insert(components[0].clone(), new_level);
+                                new_level += 1;
+                            } else {
+                                panic!();
+                            }
+                        } else {
+                            todo!();
+                        }
+                    }
+                    _ => {
+                        if n != statements.len() - 1 {
+                            panic!();
+                        }
+                        rv.push(
+                            convert_to_term(statement, &new_locals, new_level, filename).await?,
+                        );
+                    }
+                }
+                n += 1;
+            }
+            let mut r = rv.pop().unwrap();
+            for value in rv.into_iter().rev() {
+                r = Term::Let(Box::new(value), Box::new(r));
+            }
+            Ok(r)
+        }
+        AbstractSyntaxTree::List(expressions, _) => panic!(),
+        AbstractSyntaxTree::Enclosed(inner, _, _) => {
+            convert_to_term(inner, locals, level, filename).await
+        }
+        AbstractSyntaxTree::Identifier(components, _) => {
+            let name = components.join("::");
+            if name.get(..4) == Some("Type") {
+                if name.len() == 4 {
+                    return Ok(Term::Prop);
+                }
+                if let Ok(n) = name[4..].parse::<usize>() {
+                    if n > 0 {
+                        return Ok(Term::Type(n - 1));
+                    } else {
+                        panic!("No Type0");
+                    }
+                }
+            }
+            match locals.get(&name) {
+                Some(n) => Ok(Term::Variable(level - n - 1)),
+                None => kernel::get_global_index(&path::make_absolute(&name, filename))
+                    .await
+                    .map(|n| Term::Global(n)),
+            }
+        }
+        AbstractSyntaxTree::Assignment(_, _, _, _) => panic!(),
+        AbstractSyntaxTree::Application(left, right, _) => {
+            let left_term = convert_to_term(left, locals, level, filename).await?;
+            let right_term = convert_to_term(right, locals, level, filename).await?;
+            Ok(Term::Apply(Box::new(left_term), Box::new(right_term)))
+        }
+        AbstractSyntaxTree::Forall(name, var_type, value, _) => {
+            let type_term = convert_to_term(var_type, locals, level, filename).await?;
+            let mut new_locals = locals.clone();
+            if let Some(s) = name {
+                new_locals.insert(s.to_string(), level);
+            }
+            let mut new_level = level;
+            new_level += 1;
+            let value_term = convert_to_term(value, &new_locals, new_level, filename).await?;
+            Ok(Term::Forall(Box::new(type_term), Box::new(value_term)))
+        }
+        AbstractSyntaxTree::Lambda(name, var_type, value, _) => {
+            let type_term = convert_to_term(var_type, locals, level, filename).await?;
+            let mut new_locals = locals.clone();
+            new_locals.insert(name.to_string(), level);
+            let mut new_level = level;
+            new_level += 1;
+            let value_term = convert_to_term(value, &new_locals, new_level, filename).await?;
+            Ok(Term::Lambda(Box::new(type_term), Box::new(value_term)))
         }
         _ => panic!(),
     }
