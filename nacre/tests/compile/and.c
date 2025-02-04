c7d64666677463b7a78a3c2e7fff4b694f53d040
Aritz Erkiaga <aerkiaga3@gmail.com>
2025-02-05 00:13:44 +0100

    Redefine Bool

--- nacre/tests/compile/and.c
+++ nacre/tests/compile/and.c
@@ -13,10 +13,10 @@ extern Bool test(struct AClosure *self, Bool x, Bool y);
 int main() {
     struct AClosure *ac = (struct AClosure*) malloc(sizeof(struct AClosure*));
     ac->f = test;
-    int ab00 = !ac->f(ac, (Bool) !0, (Bool) !0);
-    int ab01 = !ac->f(ac, (Bool) !0, (Bool) !1);
-    int ab10 = !ac->f(ac, (Bool) !1, (Bool) !0);
-    int ab11 = !ac->f(ac, (Bool) !1, (Bool) !1);
+    int ab00 = ac->f(ac, (Bool) 0, (Bool) 0);
+    int ab01 = ac->f(ac, (Bool) 0, (Bool) 1);
+    int ab10 = ac->f(ac, (Bool) 1, (Bool) 0);
+    int ab11 = ac->f(ac, (Bool) 1, (Bool) 1);
     int r = (!ab00 && !ab01 && !ab10 && ab11);
     //printf("Test: %s\n", r? "pass" : "fail");
     return !r;
--- nacre/tests/compile/not.c
+++ nacre/tests/compile/not.c
@@ -13,8 +13,8 @@ extern Bool test(struct AClosure *self, Bool x);
 int main() {
     struct AClosure *ac = (struct AClosure*) malloc(sizeof(struct AClosure*));
     ac->f = test;
-    int nb0 = !ac->f(ac, (Bool) !0);
-    int nb1 = !ac->f(ac, (Bool) !1);
+    int nb0 = ac->f(ac, (Bool) 0);
+    int nb1 = ac->f(ac, (Bool) 1);
     int r = (nb0 && !nb1);
     //printf("Test: %s\n", r? "pass" : "fail");
     return !r;
